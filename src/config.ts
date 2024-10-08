3f2fe97391d7ecfed2cfeb2f9d2fe347a0a25031
Victor <v.safsten@gmail.com>
2024-10-08 23:13:42 +0200

    Deployment test 2

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-export const API_ADDRESS = process.env.API_ADDRESS;
+export const API_ADDRESS = process.env.API_ADDRESS || "";
 export const PORT = parseInt(process.env.PORT || "4000");
-export const MONGO_URI = process.env.MONGO_URI;
+export const MONGO_URI = process.env.MONGO_URI || "";
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "";
