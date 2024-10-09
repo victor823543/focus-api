de0f149282b648200afa36b227cc39893915353c
Victor <v.safsten@gmail.com>
2024-10-09 10:36:20 +0200

    Deployment test 5

--- dist/handlers/asyncHandler.js
+++
@@ -1,3 +0,0 @@
-export const asyncHandler = (fn) => (req, res, next) => {
-    return Promise.resolve(fn(req, res, next)).catch(next);
-};
--- dist/routes/colors.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import colorController from "../controllers/colorController.js";
-import { asyncHandler } from "../handlers/asyncHandler.js";
-const router = express.Router();
-router.get("/", asyncHandler(colorController.list));
-export default router;
