20b4304b5ba8c3722ee61063a2cb49c4a9cd21c7
Victor <v.safsten@gmail.com>
2024-10-21 11:23:00 +0200

    Update environment handling system

--- src/config.ts
+++ src/config.ts
@@ -7,9 +7,14 @@ if (["development", "devtest"].includes(NODE_ENV)) {
   dotenv.config();
 }
 
-export const MONGO_URI = ["test", "devtest"].includes(NODE_ENV)
-  ? process.env.MONGO_URI_TEST || ""
-  : process.env.MONGO_URI || "";
+export const ACTIVE_DB =
+  NODE_ENV === "devtest" || process.env.ACTIVE_DB === "test"
+    ? "test"
+    : "production";
+export const MONGO_URI =
+  NODE_ENV === "devtest"
+    ? process.env.MONGO_URI_TEST || ""
+    : process.env.MONGO_URI || "";
 export const API_ADDRESS = process.env.API_ADDRESS || "";
 export const PORT = parseInt(process.env.PORT || "4000");
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "";
--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import { ACTIVE_DB } from "../config.js";
 import testController from "../controllers/testController.js";
 import { asyncHandler } from "../handlers/asyncHandler.js";
 
 const router = express.Router();
 
-if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "devtest") {
+if (ACTIVE_DB === "test") {
   router.post("/reset-db", asyncHandler(testController.reset));
   router.post("/seed-db", asyncHandler(testController.seed));
 }
