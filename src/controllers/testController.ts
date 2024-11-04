98b7bd722cf6ccf3791fcc2ff0c127741b2737db
Victor <v.safsten@gmail.com>
2024-11-04 13:48:26 +0100

    Update test database seeding endpoint

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import mongoose from "mongoose";
 import { Category } from "../models/Category.js";
+import { Day } from "../models/Day.js";
+import { Session } from "../models/Session.js";
 import { User } from "../models/User.js";
 import { ErrorCode, SuccessCode } from "../utils/constants.js";
 import { ErrorResponse, sendValidResponse } from "../utils/sendResponse.js";
@@ -28,20 +30,50 @@ async function reset(req: Request, res: Response) {
   return sendValidResponse(res, SuccessCode.NO_CONTENT);
 }
 
-async function seed(req: Request, res: Response) {
-  const { users, categories } = req.body;
-  console.log(categories);
-  try {
-    // Clear relevant collections before seeding (optional)
-    await User.deleteMany();
-    await Category.deleteMany();
+type SeedBody = {
+  user: any;
+  globalCategories: any[];
+  userCategories: any[];
+  session: any;
+  days: any[];
+};
 
-    // Insert seed data
-    if (users && users.length > 0) {
-      await User.insertMany(users);
+async function seed(req: Request, res: Response) {
+  const { user, globalCategories, userCategories, session, days }: SeedBody =
+    req.body;
+  try {
+    if (user) {
+      const createdUser = await User.create(user);
+
+      if (userCategories && userCategories.length > 0) {
+        const newUserCategories = userCategories.map((category) => ({
+          ...category,
+          user: createdUser._id,
+        }));
+        const createdCategories = await Category.insertMany(newUserCategories);
+        const categoryIds = createdCategories.map(
+          (category: any) => category._id,
+        );
+
+        if (session) {
+          session.user = createdUser._id;
+          session.categories = categoryIds;
+          const createdSession = await Session.create(session);
+
+          if (days) {
+            const newDays = days.map((day: any) => ({
+              ...day,
+              user: createdUser._id,
+              session: createdSession._id,
+            }));
+            await Day.insertMany(newDays);
+          }
+        }
+      }
     }
-    if (categories && categories.length > 0) {
-      await Category.insertMany(categories);
+
+    if (globalCategories && globalCategories.length > 0) {
+      await Category.insertMany(globalCategories);
     }
 
     return sendValidResponse(res, SuccessCode.NO_CONTENT);
